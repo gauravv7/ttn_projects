a92f25a619fb99580fe87ef0180cc9ea28c268c2
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-02-28 12:17:18 +0530

    added error handlers: message generation and notification on form submit

--- js/function.js
+++ js/function.js
@@ -16,20 +16,83 @@ function cleanUrl(img_url) {
   if( (img_url.length == 0) || (img_url.indexOf("https://www.")==0) || (img_url.indexOf("www")==0) ) {
     throw new Error("invalid url");
   } else if(img_url.indexOf(base_url)==0){
-    if(img_url.length==base_url.length){
+    if(img_url.length==base_url.length){ // only base url is given
       throw new Error("incomplete url, resource not given");
     }
-  } else if(img_url.indexOf("/")==0){
+  } else if(img_url.indexOf("/")==0){ // url prepended with '/', in case of relative path to handle it
     img_url = img_url.substr(1, img_url.length);
     cleanUrl(img_url);
     img_url = base_url+img_url;
-  } else if(img_url.indexOf(base_url)==-1){
+  } else { // url prepended with '/', in case of relative path to handle it
     img_url = base_url+img_url;
   }
   console.log("return clean url: ",img_url);
   return img_url;
 }
 
+/*
+ desc - check required
+ params - ID attribute of element
+*/
+function validateRequired(ele){
+  return ($(ele).val().trim().length == 0)? false: true;
+}
+
+/*
+ desc - make errors for required checks
+ params - ID attribute of element
+*/
+function makeErrorMsg(ele, msg){
+    $("<span />", {
+      text: msg
+    }).appendTo($("#"+ele).parent());
+}
+
+/**
+ * [removeErrorMsg remove the associating error message HTMLElement]
+ * @param  {[String]} ele [id of input field whose error message needs to be removed]
+ * @return {[void]}     []
+ */
+function removeErrorMsg(ele){
+  var errMsg = $(ele).siblings("span");
+  if(errMsg.length){
+    errMsg.remove();
+  }
+}
+
+/**
+ * [validateForm validating for for required validations]
+ * @return {[type]} [description]
+ */
+function validateForm(){
+  var eleIds = ['genre_name', 'genre_id', 'imgUrl'];
+
+  eleIds.forEach(function(data, idx, arr){
+    var id = "#"+data;
+    removeErrorMsg(id);
+    if(!validateRequired(id)){
+      makeErrorMsg(data, $("label[for="+data+"]").text() + " is required");
+    }
+  });
+}
+
+/**
+ * [makeImgForModal makes an image element]
+ * @param  {[String]} img_url [src url of the image]
+ * @return {[JQuery Element]} [image element]
+ */
+function makeImgForModal(img_url){
+  return imgContent = $('<img />', {
+    src: img_url
+  });
+}
+
+
+/**
+ * [fetchImg fetching image for associating url]
+ * @param  {[type]} img_url [url of the image]
+ * @return {[void]}         []
+ */
 function fetchImg(img_url){
   $.ajax({
     url: img_url,
@@ -42,55 +105,66 @@ function fetchImg(img_url){
     success: function(data,textStatus, XMLHttpRequest){
       var contentType = XMLHttpRequest.getResponseHeader('content-type').toLowerCase();
       var allowedTypes = ["image/jpeg", "image/png", "image/gif"];
-      var idx = allowedTypes.indexOf(contentType); // return value to check if response is allowed
-      if(idx==-1){
+      var idx = allowedTypes.indexOf(contentType); // check if response is allowed
+      if(idx==-1){  // silently printing user message, but associaing error logs are maintained
         console.log('illegal response');
+        makeErrorMsg("imgUrl", "image cannot be loaded, resource not found");
       }
-      imgContent = $('<img />', {
-        src: img_url
-      });
-      setImgInModal(imgContent);
+
+      setImgInModal(makeImgForModal(img_url));
     },
     error: function (xhr, ajaxOptions, thrownError){
-      console.log("error occured");
+      makeErrorMsg("imgUrl", "image cannot be loaded, resource not found");
     }
   });
 }
 
-function doFetchThumb(){
-  var $self = $('#imgUrl');
-  if($self.val().trim().length){
-    setTimeout(function(){
-      try{
-        var img_url = cleanUrl($("#imgUrl").val().trim());
-        console.log("url to fetch: ",img_url);
-        fetchImg(img_url);
-      } catch(e){
-        console.log(e);
-      }
-    },100);
-  }
-}
-$('#imgUrl').on('paste, keypress, blur', function() {
-    doFetchThumb();
+$('#imgUrl').on('paste, keydown, blur', function() {
+    var $self = $(this);
+    if($self.val().trim().length){
+      setTimeout(function(){
+        try{
+          var img_url = cleanUrl($("#imgUrl").val().trim());
+          console.log("url to fetch: ",img_url);
+          fetchImg(img_url);
+        } catch(e){
+
+          removeErrorMsg($("#"+ele));
+          makeErrorMsg(data, $("label[for="+ele+"]").text() + " is required");
+        }
+      },100);
+    }
  });
+
+ function makeDeleteBtn(){
+   var delBtn = $("<button />", { type: "button", class: "delImg btn btn-default", text: "delete"});
+
+   delBtn.click(function(e){
+     $(e.target).parent().parent().remove();
+   });
+   return delBtn;
+ }
+
+function makeThumb(){
+  return $("<tr />").append($("<td />").append(imgContent)).append($('<td/>').append(makeDeleteBtn()));
+}
+
+/**
+ * [addImgRow adding thumb image to table as a row in html]
+ */
+function addImgRow(){
+  $('#imgRows').append(makeThumb());
+}
+
 $('#addPhotos').on('submit', function(e){
   console.log("on submit event handler");
   e.preventDefault();
+
+  validateForm();
+
   if(imgContent!=null){
-
-    var thumb;
-    var delBtn = $("<button />", { type: "button", class: "delImg btn btn-default", text: "delete"});
-
-    delBtn.click(function(e){
-      $(e.target).parent().parent().remove();
-    });
-    thumb = $("<tr />").append($("<td />").append(imgContent)).append($('<td/>').append(delBtn));
-    $('#imgRows').append(thumb);
-
+    addImgRow();
     clearModalContents();
-  } else{
-    doFetchThumb();
   }
 });
 
